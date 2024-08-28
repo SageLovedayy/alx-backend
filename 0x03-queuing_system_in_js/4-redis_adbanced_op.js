224d3923a4f42ef5e154d6ee373c1f2a9a8d17fe
SageLovedayy <116376154+SageLovedayy@users.noreply.github.com>
2024-08-28 23:23:18 +0100

    End redis

---
+++ 0x03-queuing_system_in_js/4-redis_adbanced_op.js
@@ -0,0 +1,37 @@
+import redis from 'redis';
+
+const client = redis.createClient({
+  host: '127.0.0.1',
+  port: 6379,
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err.message}`);
+});
+
+function createHolbertonSchoolsHash() {
+  client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+  client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+  client.hset('HolbertonSchools', 'New York', 20, redis.print);
+  client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+  client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+  client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+}
+
+function displayHolbertonSchools() {
+  client.hgetall('HolbertonSchools', (err, res) => {
+    if (err) {
+      console.error(`Error: ${err.message}`);
+    } else {
+      console.log(res);
+    }
+  });
+}
+
+createHolbertonSchoolsHash();
+displayHolbertonSchools();
+
